6315d16428b4df6b627b725e5a4f6456e398da3c
Giulia Alfonsi <electric.g@gmail.com>
2023-04-13 17:46:14 -0700

    Fixed some mocha tests

--- tests/test.mocha.js
+++ tests/test.mocha.js
@@ -3,3 +3,23 @@ describe('Mocha', function () {
     expect(123).to.equal(123);
   });
 });
+
+describe('app.js', function () {
+  it('is not testable', function () {});
+});
+
+describe('controller.js', function () {
+  it('is not testable', function () {});
+});
+
+describe('offline.js', function () {
+  it('is not testable', function () {});
+});
+
+describe('template.js', function () {
+  it('is not testable', function () {});
+});
+
+describe('view.js', function () {
+  it('is not testable', function () {});
+});
